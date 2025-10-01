e44edafed2cfd7e49063c5cb8b9b5ad469ec0178
sidi-125 <sidra.saqib712@gmail.com>
2025-10-01 17:26:36 +0500

    add file

---
+++ src/components/ui/Button/index.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const SimpleButton = () => {
+  return (
+    <button
+      style={{
+        padding: "10px 20px",
+        fontSize: "16px",
+        cursor: "pointer",
+        borderRadius: "5px",
+        border: "1px solid #ccc",
+        backgroundColor: "#f0f0f0",
+      }}
+    >
+      Submit
+    </button>
+  );
+};
+
+export default SimpleButton;
