ae0d13c0534e1e0581697b5937aaba60a6c22110
sidi-125 <sidra.saqib712@gmail.com>
2025-10-02 18:47:04 +0500

    Feature/fag 06 UI page title and breadcrumbs (#6)
    * add button files
    
    * add button files
    
    * add file
    
    * add button component
    
    * Add search button
    
    * Fix format
    
    * add title and breadcrumbs
    
    * Clena error import
    
    * Fix installations
    
    ---------
    
    Co-authored-by: SyedKamranGH <syed.kam.ahmed@gmail.com>

--- src/app/example/page.jsx
+++ src/app/example/page.jsx
@@ -1,6 +1,8 @@
 "use client";
+import React from "react";
 import CustomButton from "@/components/ui/Button";
 import { Search } from "lucide-react";
+import Breadcrumbs from "@/components/ui/Breadcrumbs/index";
 import InputField from "@/components/ui/Input";
 import Calendar from "@/components/Features/Calendar";
 import Header from "@/components/layout/Header";
@@ -319,6 +321,10 @@ function MyPage() {
           />
         </div>
       </section>
+
+      <div>
+        <Breadcrumbs breadcrumbItems={["Home", "Equipa"]} />
+      </div>
 
       {/* Card Components */}
       <section className="bg-white px-4 py-12">
